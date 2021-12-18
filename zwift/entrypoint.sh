db8b225166f84146f058137775e29b2f086c01b7
Kim Eik <kim@heldig.org>
2021-12-18 21:42:49 +0100

    new version of zwift

--- zwift/entrypoint.sh
+++ zwift/entrypoint.sh
@@ -4,24 +4,33 @@ set -x
 
 ZWIFT_HOME="$HOME/.wine/drive_c/Program Files (x86)/Zwift"
 
-if [ ! -d "$ZWIFT_HOME" ]
+mkdir -p "$ZWIFT_HOME"
+cd "$ZWIFT_HOME"
+
+if [ "$1" = "update" ]
+then
+	echo "updating zwift..."
+	wine64 start ZwiftLauncher.exe
+	wineserver -w
+	exit 0
+fi
+
+if [ ! "$(ls -A .)" ] # is directory empty?
 then
 	echo "installing zwift..."
-	mkdir -p "$ZWIFT_HOME"
-	cd "$ZWIFT_HOME"
         wget https://www.nirsoft.net/utils/runfromprocess.zip
 	unzip runfromprocess.zip	
 	wget https://cdn.zwift.com/app/ZwiftSetup.exe
 	winetricks --unattended dotnet45 win10
 	wine64 ZwiftSetup.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL
 	# Wait for Zwift to fully install and then restart container
-else
-	echo "starting zwift..."
-	cd "$ZWIFT_HOME"
-	wine64 start ZwiftLauncher.exe
-	wine64 start RunFromProcess-x64.exe ZwiftLauncher.exe ZwiftApp.exe
-	sleep 1
-	bash -c "ps aux | grep ZwiftLauncher | head -n 1 | awk '{print \$2}' | xargs kill"
+	wineserver -w
+	exit 0
 fi
 
+echo "starting zwift..."
+wine64 start ZwiftLauncher.exe
+wine64 start RunFromProcess-x64.exe ZwiftLauncher.exe ZwiftApp.exe
+sleep 1
+bash -c "ps aux | grep ZwiftLauncher | head -n 1 | awk '{print \$2}' | xargs kill"
 wineserver -w
