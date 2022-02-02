e79147c0f179d7dd6d79c638bb5ee04ac654d4f5
Kim Eik <kim@heldig.org>
2022-02-02 13:45:16 +0100

    refactored container image handling

--- zwift/zwift.sh
+++ zwift/zwift.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# The zwift version to launch
-ZWIFT_VERSION=${ZWIFT_VERSION:-latest}
-
 # The home directory to store zwift data
 ZWIFT_HOME=${ZWIFT_HOME:-$HOME/.zwift/$USER}
+
+# Set the container image to use
+IMAGE=${IMAGE:-docker.io/netbrain/zwift}
+
+# The container version
+VERSION=${VERSION:-latest}
 
 # Create the zwift home directory if not already exists
 mkdir -p $ZWIFT_HOME
 
 # Check for updated container image
-docker pull netbrain/zwift:$ZWIFT_VERSION
+docker pull $IMAGE:$VERSION
 
 # Check for proprietary nvidia driver and set correct device to use
 if [[ -f "/proc/driver/nvidia/version" ]]
@@ -30,7 +33,7 @@ CONTAINER=$(docker run \
 	-v /run/user/$UID/pulse:/run/user/1000/pulse \
 	-v $ZWIFT_HOME:/home/user/Zwift \
 	$VGA_DEVICE_FLAG \
-	netbrain/zwift:$ZWIFT_VERSION)
+	$IMAGE:$VERSION)
 	
 # Allow container to connect to X
 xhost +local:$(docker inspect --format='{{ .Config.Hostname  }}' $CONTAINER)
