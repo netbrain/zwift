2fab1e26a8622f69738a37bae9300e0be31bda15
Kim Eik <kim@heldig.org>
2022-02-02 13:08:07 +0100

    added gpu check zwift.sh

--- zwift/zwift.sh
+++ zwift/zwift.sh
@@ -12,17 +12,24 @@ mkdir -p $ZWIFT_HOME
 # Check for updated container image
 docker pull netbrain/zwift:$ZWIFT_VERSION
 
+# Check for proprietary nvidia driver and set correct device to use
+if [[ -f "/proc/driver/nvidia/version" ]]
+then
+	VGA_DEVICE_FLAG="--gpus all"
+else
+	VGA_DEVICE_FLAG="--device /dev/dri:/dev/dri"
+fi
+
 # Start the zwift container
 CONTAINER=$(docker run \
 	-d \
 	--rm \
 	--privileged \
-	--gpus all \
 	-e DISPLAY=$DISPLAY \
-	-v /dev/dri:/dev/dri \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /run/user/$UID/pulse:/run/user/1000/pulse \
 	-v $ZWIFT_HOME:/home/user/Zwift \
+	$VGA_DEVICE_FLAG \
 	netbrain/zwift:$ZWIFT_VERSION)
 	
 # Allow container to connect to X
