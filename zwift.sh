a5a01cc1d795ad4685ccf7891638ec24038da809
quietvoid <39477805+quietvoid@users.noreply.github.com>
2023-07-16 22:29:27 -0400

    Fix invalid command when using docker

--- zwift.sh
+++ zwift.sh
@@ -1,13 +1,5 @@
 #!/bin/bash +x
 
-# Use podman if available
-if [[ -x "$(command -v podman)" ]]
-then
-        CONTAINER_TOOL=podman
-else
-        CONTAINER_TOOL=docker
-fi
-
 # The home directory to store zwift data
 ZWIFT_HOME=${ZWIFT_HOME:-$HOME/.zwift/$USER}
 
@@ -19,7 +11,15 @@ VERSION=${VERSION:-latest}
 
 # Create the zwift home directory if not already exists
 mkdir -p $ZWIFT_HOME
-$CONTAINER_TOOL unshare chown $UID:$UID -R $ZWIFT_HOME
+
+# Use podman if available
+if [[ -x "$(command -v podman)" ]]
+then
+	CONTAINER_TOOL=podman
+	$CONTAINER_TOOL unshare chown $UID:$UID -R $ZWIFT_HOME
+else
+	CONTAINER_TOOL=docker
+fi
 
 # Check for updated container image
 $CONTAINER_TOOL pull $IMAGE:$VERSION
