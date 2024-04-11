79f70733fd589b894e2854abbc5606ec22a0f19e
Kim Eik <kim@heldig.org>
2024-04-11 08:29:07 +0200

    made zwift.sh configurable from file

--- zwift.sh
+++ zwift.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
+### DEFAULT CONFIGURATION ###
+
 # Set the container image to use
 IMAGE=${IMAGE:-docker.io/netbrain/zwift}
 
@@ -16,12 +18,6 @@ then
     else
         CONTAINER_TOOL=docker
     fi
-fi
-
-# Check for updated container image
-if [[ ! $DONT_PULL ]]
-then
-    $CONTAINER_TOOL pull $IMAGE:$VERSION
 fi
 
 # Check for proprietary nvidia driver and set correct device to use
@@ -43,6 +39,23 @@ then
     ZWIFT_CREDENTIALS_FLAG="--env-file ~/.config/zwift/credentials"
 fi
 
+### OVERRIDE CONFIGURATION FROM FILE ###
+
+# Check for other zwift configuration, sourced here and passed on to container aswell
+if [[ -f "~/.config/zwift/config" ]]
+then
+    ZWIFT_CONFIG_FLAG="--env-file ~/.config/zwift/config"
+    source ~/.config/zwift/config
+fi
+
+# Check for updated container image
+if [[ ! $DONT_PULL ]]
+then
+    $CONTAINER_TOOL pull $IMAGE:$VERSION
+fi
+
+### START ###
+
 # Start the zwift container
 CONTAINER=$($CONTAINER_TOOL run \
     -d \
@@ -54,6 +67,7 @@ CONTAINER=$($CONTAINER_TOOL run \
     -v zwift-$USER:/home/user/.wine/drive_c/users/user/Documents/Zwift \
     $([ "$CONTAINER_TOOL" = "podman" ] && echo '--userns=keep-id') \
     $ZWIFT_CREDENTIALS_FLAG \
+    $ZWIFT_CONFIG_FLAG \
     $VGA_DEVICE_FLAG \
     $IMAGE:$VERSION)
 
