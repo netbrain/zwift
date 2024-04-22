8fc63eabece55c02aec296033e2665a8442ede0c
Daniel Hobe <hobe@gmail.com>
2024-04-22 16:37:33 -0700

    Update /etc/pulse-client.conf to use the set UID

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -x
 
 usermod -o -u ${ZWIFT_UID} user && groupmod -o -g ${ZWIFT_GID} user
 chown -R ${ZWIFT_UID}:${ZWIFT_GID} /home/user
+sed -i "s/1000/${ZWIFT_UID}/g" /etc/pulse-client.conf
 
 gosu user:user /bin/setup_and_run_zwift
