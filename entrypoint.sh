7b9608297c37df4ababc721444c6665fe0bb4adb
Daniel Hobe <hobe@gmail.com>
2024-04-23 09:04:30 -0700

    Fix wrong pulse.conf path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 
 usermod -o -u ${ZWIFT_UID} user && groupmod -o -g ${ZWIFT_GID} user
 chown -R ${ZWIFT_UID}:${ZWIFT_GID} /home/user
-sed -i "s/1000/${ZWIFT_UID}/g" /etc/pulse-client.conf
+sed -i "s/1000/${ZWIFT_UID}/g" /etc/pulse/client.conf
 mkdir -p /run/user/${ZWIFT_UID} && chown -R user:user /run/user/${ZWIFT_UID}
 
 gosu user:user /bin/setup_and_run_zwift
