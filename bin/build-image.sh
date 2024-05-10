94d2bf2f87b33bd4664e157d1bf95a74cfd27446
Richard Holmes <richard@shedc.uk>
2024-05-10 18:25:29 +0100

    Merge Build for Podman and Docker

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -1,6 +1,45 @@
 #!/usr/bin/env bash
 set -x
-set -e
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ZWIFT_UID=$(id -u)
+ZWIFT_GID=$(id -g)
+
+# Use podman if available
+if [[ ! $CONTAINER_TOOL ]]
+then
+    if [[ -x "$(command -v podman)" ]]
+    then
+        CONTAINER_TOOL=podman
+    else
+        CONTAINER_TOOL=docker
+    fi
+fi
+
+# Update information based on Container Tool
+if [[ "$CONTAINER_TOOL" == "podman" ]]; then
+    BUILD_NAME="zwift"
+    IMAGE="localhost/zwift"
+else
+        BUILD_NAME="netbrain/zwift"
+        IMAGE="netbrain/zwift"
+fi
+
+GENERAL_FLAGS=(
+    -it
+    --privileged
+    --network bridge
+    --name zwift
+    --security-opt label=disable
+
+    -e DISPLAY=$DISPLAY
+    -e XAUTHORITY=$XAUTHORITY
+
+    -v /tmp/.X11-unix:/tmp/.X11-unix
+    -v /run/user/$UID:/run/user/$ZWIFT_UID
+)
+
 
 # Check for proprietary nvidia driver and set correct device to use
 if [[ -f "/proc/driver/nvidia/version" ]]
@@ -10,20 +49,34 @@ else
     VGA_DEVICE_FLAG="--device /dev/dri:/dev/dri"
 fi
 
-docker build -t netbrain/zwift .
-docker run -it \
-    --name zwift \
-    --privileged \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    $VGA_DEVICE_FLAG \
-    netbrain/zwift:latest
+# Initiate podman Volume with correct permissions
+if [[ "$CONTAINER_TOOL" == "podman" ]]
+then   
+    PODMAN_FLAGS=(
+        --userns keep-id:uid=$ZWIFT_UID,gid=$ZWIFT_GID
+    )
+fi
 
-BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-VERSION=$(curl -s http://cdn.zwift.com/gameassets/Zwift_Updates_Root/Zwift_ver_cur.xml | grep -oP 'sversion="\K.*?(?=")' | cut -f 1 -d ' ')
-docker commit --change="LABEL org.opencontainers.image.created=$BUILD_DATE" \
-    --change="LABEL org.opencontainers.image.version=$VERSION" \
-    --change='CMD [""]' \
-    -m "updated to version $VERSION" \
-    zwift \
-    netbrain/zwift:$VERSION
+# Cleanup on error
+trap cleanup ERR
+cleanup()
+{
+  $CONTAINER_TOOL container rm zwift
+  exit
+}
+
+
+$CONTAINER_TOOL build --force-rm -t $BUILD_NAME $SCRIPT_DIR/../.
+$CONTAINER_TOOL run ${GENERAL_FLAGS[@]} \
+    $VGA_DEVICE_FLAG \
+    ${PODMAN_FLAGS[@]} \
+    $IMAGE:latest \
+    $@
+
+
+$CONTAINER_TOOL commit zwift $BUILD_NAME:latest
+$CONTAINER_TOOL container rm zwift
+
+export IMAGE=$IMAGE
+export DONT_PULL=1
+$SCRIPT_DIR/../zwift.sh
