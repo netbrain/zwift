60c79d670e9b3375ff57968b733544dc040415cf
Richard Holmes <69856552+sHedC@users.noreply.github.com>
2024-05-21 13:22:27 +0100

    Icon put in wrong place in Install Script (#136)
    Fixes #135

--- bin/install.sh
+++ bin/install.sh
@@ -12,10 +12,11 @@ curl -s -o /usr/local/bin/zwift https://raw.githubusercontent.com/netbrain/zwift
 chmod +x /usr/local/bin/zwift
 
 mkdir -p /usr/local/share/icons/hicolor/scalable/apps
-curl -s -o /usr/local/bin/zwift https://raw.githubusercontent.com/netbrain/zwift/master/assets/hicolor/scalable/apps/Zwift\ Logogram.svg
+curl -s -o /usr/local/share/icons/hicolor/scalable/apps/zwift.svg https://raw.githubusercontent.com/netbrain/zwift/master/assets/hicolor/scalable/apps/Zwift%20Logogram.svg
 
 mkdir -p /usr/local/share/applications
 curl -s -o /usr/local/share/applications/Zwift.desktop https://raw.githubusercontent.com/netbrain/zwift/master/assets/Zwift.desktop
+
 
 if [ "$(echo $PATH | grep /usr/local/bin)" = "" ]; then
     echo "WARNING: Could not find /usr/local/bin on the \$PATH, you might need to add it."
