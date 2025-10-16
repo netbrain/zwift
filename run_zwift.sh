c57bbc47937d780fbac7bc5ba43d5389af18d490
Glenn Van Loon <glenn.vanloon@gmail.com>
2025-10-16 16:47:48 +0200

    Fix shellcheck warnings

--- run_zwift.sh
+++ run_zwift.sh
@@ -10,7 +10,7 @@ if [ ! -d "$ZWIFT_HOME" ]; then
     exit 1
 fi
 
-if [ ! -z $ZWIFT_OVERRIDE_RESOLUTION ]; then
+if [[ -n $ZWIFT_OVERRIDE_RESOLUTION ]]; then
     if [ -f "$ZWIFT_PREFS" ]; then
         echo "Setting zwift resolution to $ZWIFT_OVERRIDE_RESOLUTION."
         UPDATED_PREFS=$(awk -v resolution="$ZWIFT_OVERRIDE_RESOLUTION" '{
@@ -31,23 +31,21 @@ wine start ZwiftLauncher.exe SilentLaunch
 LAUNCHER_PID_HEX=$(winedbg --command "info proc" | grep -P "ZwiftLauncher.exe" | grep -oP "^\s\K.+?(?=\s)")
 LAUNCHER_PID=$((16#$LAUNCHER_PID_HEX))
 
-if [[ ! -z "$ZWIFT_USERNAME" ]] && [[ ! -z "$ZWIFT_PASSWORD" ]];
-then
+if [[ -n $ZWIFT_USERNAME ]] && [[ -n $ZWIFT_PASSWORD ]]; then
     echo "authenticating with zwift..."
-    wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe --token=$(zwift-auth)
+    wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe --token="$(zwift-auth)"
 else
     wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe
 fi
 
 sleep 3
 
-until pgrep -f ZwiftApp.exe &> /dev/null
-do
+until pgrep -f ZwiftApp.exe &> /dev/null; do
     echo "Waiting for zwift to start ..."
     sleep 1
 done
 
-echo "Killing uneccesary applications"
+echo "Killing unnecessary applications"
 # ZwiftLauncher can exit on it's own before getting this far, so try to kill it
 # and then always return 0 so the script does not exit non 0 here and cause the
 # container to exit.  See https://github.com/netbrain/zwift/issues/210
@@ -55,4 +53,8 @@ pkill ZwiftLauncher || true
 pkill ZwiftWindowsCra
 pkill -f MicrosoftEdgeUpdate
 
-[ -z "$ZWIFT_NO_GAMEMODE" ] && /usr/games/gamemoderun wineserver -w || wineserver -w
+if [ -z "$ZWIFT_NO_GAMEMODE" ]; then
+    /usr/games/gamemoderun wineserver -w
+else
+    wineserver -w
+fi
