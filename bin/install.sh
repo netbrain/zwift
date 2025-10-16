95fe86591ac3f4b25da5f2fc7ea6c865303c4636
Glenn Van Loon <glenn.vanloon@gmail.com>
2025-10-16 16:37:55 +0200

    Fix shellcheck warnings

--- bin/install.sh
+++ bin/install.sh
@@ -1,47 +1,47 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ $EUID -eq 0 ]]; then
-  root_bin=/usr/local/bin
-  root_share=/usr/local/share
+  ROOT_BIN=/usr/local/bin
+  ROOT_SHARE=/usr/local/share
 else
   # user install
-  root_bin="${XDG_BIN_HOME:-$HOME/.local/bin}"
-  root_share="${XDG_DATA_HOME:-$HOME/.local/share}"
+  ROOT_BIN="${XDG_BIN_HOME:-$HOME/.local/bin}"
+  ROOT_SHARE="${XDG_DATA_HOME:-$HOME/.local/share}"
 fi
 
 echo "Installing zwift to:"
-echo "  binaries → ${root_bin}"
-echo "  data     → ${root_share}"
+echo "  binaries → $ROOT_BIN"
+echo "  data     → $ROOT_SHARE"
 
-read -p "Press ENTER to continue or Ctrl-C to abort…"
+read -r -p "Press ENTER to continue or Ctrl-C to abort…"
 
 # create dirs
-mkdir -p "${root_bin}"
-mkdir -p "${root_share}/icons/hicolor/scalable/apps"
-mkdir -p "${root_share}/applications"
+mkdir -p "$ROOT_BIN"
+mkdir -p "$ROOT_SHARE/icons/hicolor/scalable/apps"
+mkdir -p "$ROOT_SHARE/applications"
 
 # download
 curl -fsSL \
-  -o "${root_bin}/zwift" \
+  -o "$ROOT_BIN/zwift" \
   https://raw.githubusercontent.com/netbrain/zwift/master/zwift.sh
-chmod 755 "$root_bin/zwift"
+chmod 755 "$ROOT_BIN/zwift"
 
 curl -fsSL \
-  -o "${root_share}/icons/hicolor/scalable/apps/zwift.svg" \
+  -o "$ROOT_SHARE/icons/hicolor/scalable/apps/zwift.svg" \
   https://raw.githubusercontent.com/netbrain/zwift/master/assets/hicolor/scalable/apps/Zwift%20Logogram.svg
 
 curl -fsSL \
-  -o "${root_share}/applications/Zwift.desktop" \
+  -o "$ROOT_SHARE/applications/Zwift.desktop" \
   https://raw.githubusercontent.com/netbrain/zwift/master/assets/Zwift.desktop
 
 
 # warn if bin dir not in PATH
 if ! case ":$PATH:" in
-      *":$root_bin:"*) true  ;;
+      *":$ROOT_BIN:"*) true  ;;
       *)               false ;;
     esac; then
   cat <<WARN
-Warning: $root_bin is not in your PATH.
+Warning: $ROOT_BIN is not in your PATH.
 You may need to add it to your PATH for the zwift command to work
 WARN
 fi
