f2d8c23bdc2c6a8a113ad8634b310e7e8498d945
Glenn Van Loon <glenn.vanloon@gmail.com>
2025-10-16 16:44:11 +0200

    Fix shellcheck warnings

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -1,19 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -x
 
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 ZWIFT_UID=$(id -u)
 ZWIFT_GID=$(id -g)
 
 # Use podman if available
-if [[ ! $CONTAINER_TOOL ]]
-then
-    if [[ -x "$(command -v podman)" ]]
-    then
-        CONTAINER_TOOL=podman
+if [[ ! $CONTAINER_TOOL ]]; then
+    if [[ -x "$(command -v podman)" ]]; then
+        CONTAINER_TOOL="podman"
     else
-        CONTAINER_TOOL=docker
+        CONTAINER_TOOL="docker"
     fi
 fi
 
@@ -22,8 +19,8 @@ if [[ "$CONTAINER_TOOL" == "podman" ]]; then
     BUILD_NAME="zwift"
     IMAGE="localhost/zwift"
 else
-        BUILD_NAME="netbrain/zwift"
-        IMAGE="netbrain/zwift"
+    BUILD_NAME="netbrain/zwift"
+    IMAGE="netbrain/zwift"
 fi
 
 GENERAL_FLAGS=(
@@ -32,14 +29,14 @@ GENERAL_FLAGS=(
     --name zwift
     --security-opt label=type:container_runtime_t
     --device /dev/dri
-    --hostname $HOSTNAME
+    --hostname "$HOSTNAME"
 
-    -e DISPLAY=$DISPLAY
-    -e XAUTHORITY=$XAUTHORITY
-    -e CONTAINER=$CONTAINER_TOOL
+    -e DISPLAY="$DISPLAY"
+    -e XAUTHORITY="$XAUTHORITY"
+    -e CONTAINER="$CONTAINER_TOOL"
 
     -v /tmp/.X11-unix:/tmp/.X11-unix
-    -v /run/user/$UID:/run/user/$ZWIFT_UID
+    -v /run/user/$UID:"/run/user/$ZWIFT_UID"
 )
 
 
@@ -52,10 +49,10 @@ else
 fi
 
 # Initiate podman Volume with correct permissions
-if [[ "$CONTAINER_TOOL" == "podman" ]]; then 
+if [[ "$CONTAINER_TOOL" == "podman" ]]; then
     # Add ipc host to deal with an SHM issue on some machines.
     PODMAN_FLAGS=(
-        --userns keep-id:uid=$ZWIFT_UID,gid=$ZWIFT_GID
+        --userns "keep-id:uid=$ZWIFT_UID,gid=$ZWIFT_GID"
     )
 fi
 
@@ -63,22 +60,22 @@ fi
 trap cleanup ERR
 cleanup()
 {
-  $CONTAINER_TOOL container rm zwift
-  exit
+    $CONTAINER_TOOL container rm zwift
+    exit
 }
 
 
-$CONTAINER_TOOL build --force-rm -t $BUILD_NAME $SCRIPT_DIR/../.
-$CONTAINER_TOOL run ${GENERAL_FLAGS[@]} \
-    $VGA_DEVICE_FLAG \
-    ${PODMAN_FLAGS[@]} \
-    $IMAGE:latest \
-    $@
+$CONTAINER_TOOL build --force-rm -t "$BUILD_NAME" "$SCRIPT_DIR/../."
+$CONTAINER_TOOL run "${GENERAL_FLAGS[@]}" \
+    "$VGA_DEVICE_FLAG" \
+    "${PODMAN_FLAGS[@]}" \
+    "$IMAGE:latest" \
+    "$@"
 
 
-$CONTAINER_TOOL commit zwift $BUILD_NAME:latest
+$CONTAINER_TOOL commit zwift "$BUILD_NAME:latest"
 $CONTAINER_TOOL container rm zwift
 
-export IMAGE=$IMAGE
+export IMAGE="$IMAGE"
 export DONT_PULL=1
-$SCRIPT_DIR/../zwift.sh
+"$SCRIPT_DIR/../zwift.sh"
