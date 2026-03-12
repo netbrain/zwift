8f390649256d0aec8c581b00d16fb2a19e443d16
Kim Eik <kim@heldig.org>
2026-03-12 11:29:30 +0100

    Update src/entrypoint.sh
    Co-authored-by: Glenn <glenn.vanloon@gmail.com>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -129,8 +129,7 @@ if [[ ${CONTAINER_TOOL} == "docker" ]]; then
         # This avoids a costly recursive find on every normal startup
         local target="${1:?}"
         local result
-        result="$(find "${target}" -maxdepth 0 \( ! -user user -o ! -group user \) -print 2> /dev/null)" || true
-        [[ -d ${target} ]] && [[ -n ${result} ]]
+        [[ -d ${target} ]] && result="$(find "${target}" -maxdepth 0 \( ! -user user -o ! -group user \) -print 2> /dev/null)" && [[ -n ${result} ]]
     }
 
     update_ownership() {
