6bacffc765aee1ec306095726ef6eb4c5df7df0a
Antoine Vandecrème <antoine@vandecreme.net>
2026-07-26 18:55:39 +0200

    Simplify FHS building

--- nix/zwift-fhs-package.nix
+++ nix/zwift-fhs-package.nix
@@ -25,155 +25,28 @@ let
   zwift-fhs = pkgs.buildFHSEnv {
     name = "zwift-fhs";
 
-    # Enable 32-bit support for Wine
-    multiArch = true;
-
     targetPkgs =
       pkgs: with pkgs; [
-        # Wine (staging has better compatibility than devel/unstable)
+        # Wine
         wineWow64Packages.stagingFull
         winetricks
 
-        # Graphics - Vulkan
-        vulkan-loader
-        vulkan-tools
-
-        # Graphics - OpenGL/Mesa
-        mesa
-        libGL
-        libGLU
-        libglvnd
-
-        # X11 libraries
-        libx11
-        libxext
-        libxrandr
-        libxrender
-        libxcursor
-        libxfixes
-        libxi
-        libxcomposite
-        libxdamage
-        libxtst
-        libxscrnsaver
-        libxcb
-        libice
-        libsm
-        xrandr
-
-        # Wayland
-        wayland
-        libxkbcommon
-        wayland-protocols
-
-        # Audio
-        pipewire
-        alsa-lib
-        alsa-plugins
-
-        # System libraries
-        glib
-        glibc
-        dbus
-        systemd
-        fontconfig
-        freetype
-
-        # Networking
-        curl
+        # Networking (wget used by install script; cacert referenced in profile)
         wget
         cacert
-
-        # Process utilities
-        procps
-        coreutils
-        gnugrep
-        gawk
-        gnused
-        findutils
-        file
-        which
-        bash
 
         # Tools required by winetricks
         cabextract
         p7zip
         unzip
-        zenity
 
         # Game mode
         gamemode
-
-        # Additional libraries often needed by Wine
-        openssl
-        gnutls
-        libgpg-error
-        sqlite
-        libxml2
-        ncurses
-        zlib
-        libpng
-        libjpeg
-        SDL2
-        openal
-
-        # Desktop integration
-        gsettings-desktop-schemas
-        gtk3
-        gdk-pixbuf
 
         # Our wrapper script
         zwift-nix-fhs
       ];
 
-    # 32-bit packages for Wine compatibility
-    multiPkgs =
-      pkgs: with pkgs; [
-        # Graphics
-        vulkan-loader
-        mesa
-        libGL
-        libGLU
-        libglvnd
-
-        # X11
-        libx11
-        libxext
-        libxrandr
-        libxrender
-        libxcursor
-        libxfixes
-        libxi
-        libxcomposite
-        libxdamage
-        libxtst
-        libxcb
-
-        # Audio
-        alsa-lib
-        alsa-plugins
-
-        # System
-        glib
-        glibc
-        dbus
-        fontconfig
-        freetype
-
-        # Wine dependencies
-        gnutls
-        openssl
-        ncurses
-        zlib
-        libpng
-        libjpeg
-        SDL2
-        openal
-
-        # GTK
-        gtk3
-        gdk-pixbuf
-      ];
 
     profile = ''
       ${if winePrefix != "" then ''
@@ -189,19 +62,8 @@ let
       export NIX_SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
       export CURL_CA_BUNDLE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
 
-      # Set up Vulkan ICD path
-      export VK_ICD_FILENAMES="''${VK_ICD_FILENAMES:-}"
-
       # Ensure HOME is set (required by Wine)
       export HOME="''${HOME:-/tmp}"
-
-      # Set up XDG directories
-      export XDG_DATA_HOME="''${XDG_DATA_HOME:-$HOME/.local/share}"
-      export XDG_CONFIG_HOME="''${XDG_CONFIG_HOME:-$HOME/.config}"
-      export XDG_CACHE_HOME="''${XDG_CACHE_HOME:-$HOME/.cache}"
-
-      # Create Wine directories if needed
-      mkdir -p "''${WINEPREFIX}" 2>/dev/null || true
 
       # Set WINE and WINE64 explicitly for winetricks
       # Modern Wine uses a unified binary, but winetricks expects wine64
